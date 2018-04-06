1535cf325ba49ab964ab56849690c9b56e11fa51
ncuevas <nicolas.cuevas@greencodesoftware.com>
2018-04-06 05:59:43 -0300

    Add functions for vector_dinamico

--- vector_dinamico.c
+++ vector_dinamico.c
@@ -1,41 +1,46 @@
 #include "vector_dinamico.h"
-
+#include <stdbool.h>
 
 // Funciones del alumno.
-// Crea un vector de tamaño tam
-// Post: vector es una vector vacío de tamaño tam
-vector_t* vector_crear(size_t tam);
 
 // Destruye el vector
 // Pre: el vector fue creado
 // Post: se eliminaron todos los elementos del vector
-void vector_destruir(vector_t* vector);
-
-// Cambia el tamaño del vector
-// Pre: el vector fue creado
-// Post: el vector cambió de tamaño a nuevo_tam y devuelve true
-// o el vector queda intacto y devuelve false si no se pudo cambiar el tamaño
-// a nuevo_tam
-bool vector_redimensionar(vector_t* vector, size_t nuevo_tam);
+void vector_destruir(vector_t* vector){
+    for (int i=0; i < vector->tam; i++){
+        int* valor = &vector->datos[i];
+        free(valor);
+    }
+    free(vector);
+    return;
+}
 
 // Almacena en valor el dato guardado en la posición pos del vector
 // Pre: el vector fue creado
 // Post: se almacenó en valor el dato en la posición pos. Devuelve false si la
 // posición es inválida (fuera del rango del vector, que va de 0 a tamaño-1)
-bool vector_obtener(vector_t* vector, size_t pos, int* valor);
+bool vector_obtener(vector_t* vector, size_t pos, int* valor){
+    if (((int)vector->tam - 1) < (int)pos) return false;
+    *valor = vector->datos[pos];
+    return true;
+}
 
 // Almacena el valor en la posición pos
 // Pre: el vector fue creado
 // Post: se almacenó el valor en la posición pos. Devuelve false si la posición
 // es inválida (fuera del rango del vector, que va de 0 a tamaño-1) y true si
 // se guardó el valor con éxito.
-bool vector_guardar(vector_t* vector, size_t pos, int valor);
+bool vector_guardar(vector_t* vector, size_t pos, int valor){
+    if (((int)vector->tam - 1) < (int)pos) return false;
+    vector->datos[pos] = valor;
+    return true; 
+}
 
 // Devuelve el tamaño del vector
 // Pre: el vector fue creado
-size_t vector_obtener_tamanio(vector_t* vector);
-
-#endif // _VECTOR_DINAMICO_H
+size_t vector_obtener_tamanio(vector_t* vector){
+    return vector->tam;
+}
 
 
 // Funciones implementadas por la catedra.
